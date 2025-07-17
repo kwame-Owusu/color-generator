887be633bf0d2c657ba3f15bf16f54a0a7bdd52d
Kwame Owusu <alfred.owusu@zing.dev>
2025-07-17 15:56:20 +0100

    able to generate colors via the button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
-function ColorDivs() {
-  const colors = ["#7B4019", "#FF7D29", "#FFBF78", "#FFEEA9"]; // divs
+function ColorDivs({colors}) {
   return (
     <div className="container">
       {colors.map((color, index) => {
@@ -10,7 +9,7 @@ function ColorDivs() {
           <div
             key={index}
             className={`color-${index + 1} color`}
-            style={{ backgroundColor: color }}
+            style={{ backgroundColor: colors[index]}}
           >
             {color}
           </div>
@@ -20,25 +19,30 @@ function ColorDivs() {
   );
 }
 
-function handleGenerate() {
-  let letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * letters.length)];
-  }
-  return color;
-}
-
 function handleCopy() {
   console.log("copying colors......");
 }
 
 function App() {
-  const [currentColor, setColor] = useState([]);
+  const colors = ["#7B4019", "#FF7D29", "#FFBF78", "#FFEEA9"];
+  const [currentColors, setColors] = useState(colors);
 
+  function handleGenerate() {
+    let letters = "0123456789ABCDEF";
+    const colors = [];
+    for (let i = 0; i < 4; i++) {
+      let color = "#";
+      for (let j = 0; j < 6; j++) {
+        color += letters[Math.floor(Math.random() * letters.length)];
+      }
+      colors.push(color);
+    }
+    console.log(colors);
+    setColors(colors);
+  }
   return (
     <>
-      <ColorDivs />
+      <ColorDivs colors={currentColors}/>
       <button className="generate-btn" onClick={() => handleGenerate()}>
         Generate
       </button>
