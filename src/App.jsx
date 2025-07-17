c27c5074ba3d555a58f3c783fa4c5ecbd881298d
Kwame Owusu <alfred.owusu@zing.dev>
2025-07-17 14:54:33 +0100

    layout coming along

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+function ColorDivs() {
+  return (
+    <div className="container">
+      <div className="color-1 color">1</div>
+      <div className="color-2 color">2</div>
+      <div className="color-3 color">3</div>
+      <div className="color-4 color">4</div>
+    </div>
+  );
+}
+
+function generateColor(){
+  console.log("generating color......");
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <ColorDivs />
+    <button className="generate-btn" onClick={() => generateColor()}>Generate</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
